91441781858dfbb6fcd4d55ac4bbde216158ba4a
Chris W. Burke <chris@chriswburke.com>
2018-07-08 18:24:19 -0400

    Removed gulp-util
    Replaced log with signale
    Replaced pluginerror with newer plugin-error module

--- index.js
+++ index.js
@@ -1,91 +1,83 @@
-var gutil = require('gulp-util');
-var path = require('path');
-var through = require('through2');
-var chalk = require('chalk');
-var request = require('request');
-var fs = require('fs');
-
-// Colors
-var errorbold = chalk.bold.red;
-var error = chalk.bold.red;
-var allgood = chalk.green;
-var allgoodbold = chalk.bold.green;
-var gray = chalk.gray;
+const path = require('path')
+const through = require('through2')
+const signale = require('signale')
+const request = require('request')
+const fs = require('fs')
 
 function slang(opt) {
     // if no options passed, create options object
     if (!opt) opt = {};
     // Set defaults for options
-    var HOST = opt.host || 'localhost';
-    var PORT = opt.port || 4502;
-    var USER = opt.username || 'admin';
-    var PASS = opt.password || 'admin';
-    var URL;
+    const HOST = opt.host || 'localhost'
+    const PORT = opt.port || 4502
+    const USER = opt.username || 'admin'
+    const PASS = opt.password || 'admin'
 
     // return stream to gulp
-    var stream = through.obj(function(file, enc, cb) {
+    const stream = through.obj((file, enc, cb) => {
         if (file.isNull()) {
-            cb(new gutil.PluginError('gulp-slang', 'No file passed'));
-            return;
+            cb(new PluginError('gulp-slang', 'No file passed'));
+            return
         }
 
         if (file.isStream()) {
-            cb(new gutil.PluginError('gulp-slang', 'Streaming not supported'));
-            return;
+            cb(new PluginError('gulp-slang', 'Streaming not supported'));
+            return
         }
         // if jcr_root is in file system path, remove before setting destination
-        var destPath = file.path;
+        let destPath = file.path;
         if (path.dirname(destPath).indexOf('jcr_root') !== -1) {
             destPath = destPath.substring(path.dirname(destPath)
                 .indexOf('jcr_root') + 9);
         }
 
         // create full URL for curl path
-        URL = 'http://' + USER + ':' + encodeURIComponent(PASS) + '@' +
-            HOST + ':' + PORT + '/' + path.dirname(destPath) + ".json";
-
-        var options = {
+        const URL = `http://${USER}:${encodeURIComponent(PASS)}@${HOST}:${PORT}/${path.dirname(destPath)}.json`
+        const options = {
             url: URL,
             method: 'POST',
             headers: {
                 'Accept': 'application/json'
             }
-        };
+        }
 
-        function optionalCallback (err, httpResponse, responseBody) {
+        function handler (err, response, body) {
+            //console.log(err, response && response.statusCode)
             if (err) {
-                gutil.log(errorbold('File Upload Failed'));
+                console.log('error')
+                signale.error('File Upload Failed')
+                return
             }
 
             try {
-                var response = JSON.parse( responseBody );
+                const response = JSON.parse( body )
 
-                var status = response['status.code'] ? response['status.code'] : 201;
-                var message = response['status.message'] ? response['status.message'] : 'File Created';
-                var location = response.location ? response.location : file.path;
+                const status = response['status.code'] ? response['status.code'] : 201
+                const message = response['status.message'] ? response['status.message'] : 'File Created'
+                const location = response.location ? response.location : file.path
 
                 if (status === 200 || status === 201 ) {
-                    gutil.log( allgoodbold('File Upload Successful to port ') + allgood(PORT) + allgood(" : ") +
-                        allgood(status) + ' - ' + allgood(message));
-                    gutil.log(gray('Uploaded To: ') + gray(location));
+                    signale.success(`File Upload Successful to port ${PORT} ${status} - ${message}`)
+                    signale.info(`Uploaded to: ${location}`)
                 } else {
-                    gutil.log(errorbold('File Upload Failed: ') +
-                        error(status) + ' - ' + error(message));
+                    signale.error(`File Upload Failed`)
+                    signale.info(`${(status == 500) ? 'File destination may not exist' : message}`)
                 }
             } catch(err) {
                 // generic error most often given with username and password
-                gutil.log(errorbold('File Upload Failed - Check Username and Password'));
+                if (response && response.statusCode == 401){
+                    signale.error('[UNAUTHORIZED] File Upload Failed - Check Username and Password')
+                }
             }
 
             // deferred.resolve( responseBody);
-            cb(null, file);
+            cb(null, file)
 
-        };
+        }
 
-        var r = request(options , optionalCallback).auth(USER, PASS);
+        const r = request(options, handler).auth(USER, PASS)
 
-        var form = r.form();
-
+        const form = r.form()
         form.append('*', fs.createReadStream(file.path));
         form.append('@TypeHint', "nt:file");
     });
